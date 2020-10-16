d2554bc2a844131de0d0b4329e27eb398d31eb94
Roman Gomolko <rgomolko@gmail.com>
2020-10-16 13:43:56 +0300

    Prioritize module.exports above AMD's define

--- tc-string-parse.js
+++ tc-string-parse.js
@@ -1,11 +1,13 @@
 "use strict";
 
 (function (root, factory) {
-    if (typeof define === "function" && define.amd) {
-        define([], factory);
-    } else if (typeof module === "object" && module.exports) {
+    if (typeof module === "object" && module.exports) {
         module.exports = factory();
-    } else {
+    } 
+    else if (typeof define === "function" && define.amd) {
+        define([], factory);
+    }
+    else {
         root.TCStringParse = factory();
     }
 }(typeof self === "undefined" ? this : self, function () {
